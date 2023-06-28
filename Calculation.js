c15f23d650011d68969a18c55be19068d24d775c
tom <83908974+goat6@users.noreply.github.com>
2023-06-28 08:08:03 +0100

    Added upper bound to calculation

--- Calculation.js
+++ Calculation.js
@@ -1,30 +1,26 @@
 // Wilson's Confidence Interval Thomas Yonaha-McCoy
-let sampleArray = [{ id: 1, likes: 3, dislikes: 7 }, { id: 2, likes: 4, dislikes: 0 }, { id: 3, likes: 25, dislikes: 30 }, { id: 4, likes: 10, dislikes: 2 },] //array of comments (objects) which would be fetched as JSON from DB
-lowerBoundary = (likes,dislikes,id) => { //intervalFunction (lower boundary)
+let sampleArray = [ //sample array of comments (objects) which would be fetched as JSON from DB
+  { id: 1, likes: 3, dislikes: 7 },
+  { id: 2, likes: 4, dislikes: 0 },
+  { id: 3, likes: 25, dislikes: 30 },
+  { id: 4, likes: 10, dislikes: 2 },] 
+
+wilsonCI = (likes, dislikes, id) => { //intervalFunction (lower boundary)
   n = likes + dislikes //Sample size
   z = 1.64485 //90% (quantile of standard normal distribution)
   p = likes / n //like to dislike ratio (sample success rate)
-  
-  // calculationResult = ((p + 1 / (2 * n) * (z**2)) - (z * Math.sqrt((p * (1 - p) / n) + ((z**2))))) / (4 * (n**2))  //transcription of equation
 
-  topPart = p + 1 / (2 * n) * (z**2)-z * Math.sqrt((p * (1 - p) / n) + ((z**2) / (4 * (n**2)))) //top part of transcribed equation
-  bottomPart = 1 + 1 / n * (z**2) //bottom part of transcribed equation
+  //parts of transcribed equation - now seperated to three parts for efficiency
+  leftPart = p + 1 / (2 * n) * (z**2)
+  rightPart = z * Math.sqrt((p * (1 - p) / n) + ((z**2) / (4 * (n**2)))) 
+  bottomPart = 1 + 1 / n * (z ** 2) 
   
-  calculationResult = topPart/bottomPart //final result
+  lowerBoundResult = (leftPart - rightPart) / bottomPart
+  upperBoundResult = (leftPart + rightPart) / bottomPart
 
-  return calculationResult
+  return { lowerBoundResult,upperBoundResult, id } //object containing new data
 }
-// upperBoundary = (likes,dislikes,id) => { //intervalFunction (lower boundary)
-//   n = likes + dislikes //Sample size
-//   z = 1.2816 //90% (quantile of standard normal distribution)
-//   p = likes / n //like to dislike ratio (sample success rate)
-  
-//   calculationResult = (p + ((z ^ 2) / (2 * (n))) + z * Math.sqrt((p * (1 - p) / n) + ((z ^ 2) / (4 * (n ^ 2)))) / (1 + ((z ^ 2) / n))) //transcription of equation
 
-//   return { calculationResult, id }
-// }
-
-const lower = sampleArray.map(element => lowerBoundary(element.likes, element.dislikes,element.id)) //mapping all results to a new array
-console.log(lower)
-// const higher = sampleArray.map(element => upperBoundary(element.likes, element.dislikes, element.id))
-// console.log(higher)
+//mapping all results to a new array
+const results = sampleArray.map(element => wilsonCI(element.likes, element.dislikes,element.id)) 
+console.log(results)
