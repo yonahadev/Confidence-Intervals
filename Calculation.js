463dbd7290ee09e9386ecaed9fcb228928663d16
tom <83908974+goat6@users.noreply.github.com>
2023-06-28 11:05:56 +0100

    Added wald's CI

--- Calculation.js
+++ Calculation.js
@@ -1,9 +1,9 @@
 // Wilson's Confidence Interval Thomas Yonaha-McCoy
 let sampleArray = [ //sample array of comments (objects) which would be fetched as JSON from DB
-  { id: 1, likes: 3, dislikes: 7 },
-  { id: 2, likes: 4, dislikes: 0 },
-  { id: 3, likes: 25, dislikes: 30 },
-  { id: 4, likes: 10, dislikes: 2 },] 
+  { id: 1, likes: 1, dislikes: 0 },
+  { id: 2, likes: 10, dislikes: 1 },
+  { id: 3, likes: 40, dislikes: 20 },
+  { id: 4, likes: 100, dislikes: 40 },] 
 
 wilsonCI = (likes, dislikes, id) => { //intervalFunction (lower boundary)
   n = likes + dislikes //Sample size
@@ -17,10 +17,24 @@ wilsonCI = (likes, dislikes, id) => { //intervalFunction (lower boundary)
   
   lowerBoundResult = (leftPart - rightPart) / bottomPart
   upperBoundResult = (leftPart + rightPart) / bottomPart
+  meanResult = ((lowerBoundResult + upperBoundResult) / 2).toFixed(5)
 
-  return { lowerBoundResult,upperBoundResult, id } //object containing new data
+  return { lowerBoundResult,upperBoundResult,meanResult, id } //object containing new data
 }
 
+waldCI = (likes, dislikes, id) => {
+  n = likes + dislikes //Sample size
+  z = 1.64485 //90% (quantile of standard normal distribution)
+  p = likes / n //like to dislike ratio (sample success rate)
+
+  lowerBoundResult = p + z * Math.sqrt(p * (1 - p) / n)
+  upperBoundResult = p - z * Math.sqrt(p * (1 - p) / n)
+  meanResult = ((lowerBoundResult + upperBoundResult) / 2).toFixed(5)
+  
+  return { lowerBoundResult,upperBoundResult,meanResult, id } //object containing new data
+}
+
+
 //mapping all results to a new array
-const results = sampleArray.map(element => wilsonCI(element.likes, element.dislikes,element.id)) 
+const results = sampleArray.map(element => waldCI(element.likes, element.dislikes,element.id)) 
 console.log(results)
